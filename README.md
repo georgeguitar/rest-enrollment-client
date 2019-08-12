c1f66731c4a62d9dd3c831d0ff702e92bc166d47
Juan Navarro <jdna0615@gmail.com>
2019-08-11 22:04:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rest-enrollment-client
---
+++ enrollment/enrollment.php
@@ -0,0 +1,144 @@
+<?php
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+include('../config.inc.php');
+/*
+* Listar todos los registros de la direccion API
+* especificamente a lo concerniente a los que son todos los registros
+*/
+
+use GuzzleHttp\Client;
+
+$client = new Client();
+$token = $_SESSION['id_token'];
+
+$headers = [
+    'Authorization' => 'Bearer '. $token,
+    'Accept' => 'application/json'
+];
+
+$res = $client->request(
+    'GET',
+    DIRECCION_BASE.'/api/enrollments',
+    [ 'headers' => $headers ]
+);
+
+/*
+* Si todo va bien obtenemos los registros y mostramos en pantalla
+* En este caso especifico sabe 200 en el codigo de estado
+*/
+if ($res->getStatusCode() == '200'){
+  $data = json_decode($res->getBody(), true);
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Starter Template · Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+<link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="../starter-template.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#">TAREA API REST</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="../menu.php">Home <span class="sr-only">(current)</span></a>
+      </li>      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Entities</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+        <a class="dropdown-item" href="../student/student.php">Student</a>
+          <a class="dropdown-item" href="enrollment.php">Enrollment</a>
+          <a class="dropdown-item" href="../program/program.php">Program</a>
+        </div>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0" action="../logout.php" method="post">      
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Logout</button>
+    </form>
+  </div>
+</nav>
+
+<main role="main" class="container">
+
+  <div class="starter-template">
+    <h1>Enrollments</h1>    
+    <div class="text-right">
+        <button type="button" class="btn btn-primary" type="submit" onclick="location.href='insertEnrollment.php'">Insert Enrollment</button>
+        <p></p>
+    </div>
+    <table class="table table-sm table-dark">
+        <thead class="thead-light">
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Year</th>
+            <th scope="col">Period</th>
+            <th scope="col">Level</th>
+            <th scope="col">Date Enrollment</th>
+            <th scope="col">Student</th>
+            <th scope="col">Program</th>
+            <th scope="col">Options</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php 
+        foreach ($data as $key => $value) {
+        ?>
+            <tr>
+            <th scope="row"><?php echo $key+1; ?></th>
+            <td><?php echo $value['year']; ?></td>
+            <td><?php echo $value['period']; ?></td>
+            <td><?php echo $value['level']; ?></td>
+            <td><?php echo $value['dateEnrollment']; ?></td>
+            <td><?php echo $value['student']['name']; ?></td>
+            <td><?php echo $value['program']['name']; ?></td>
+            <td><a href="viewEnrollment.php?id=<?php echo $value['id']; ?>"><button type="button" class="btn btn-info">View</button></a><a href="updateEnrollment.php?id=<?php echo $value['id']; ?>"><button type="button" class="btn btn-primary">Edit</button></a><a href="deleteEnrollment.php?id=<?php echo $value['id']; ?>"><button type="button" class="btn btn-danger">Delete</button></a></td>
+            </tr>
+        <?php 
+        }
+        ?>
+        </tbody>
+    </table>
+  </div>
+
+</main><!-- /.container -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="js/jquery-slim.min.js"><\/script>')</script><script src="../js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script></body>
+</html>
+<?php
+}
+?>
